0bddf2120f5b1b27c4e378fd1466b641cfe7d48e
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2022-03-07 22:41:22 -0300

    adapting to work with environments and profiles

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "adrianorodrigues.com.br/investment-categories/framework/entrypoint/rest"
+import (
+	"adrianorodrigues.com.br/investment-categories/application"
+	"adrianorodrigues.com.br/investment-categories/framework/entrypoint/rest"
+)
 
 // função principal
 func main() {
+	application.NewApplication().Start()
 	rest.HttpServerSingleton().Init()
 
 }
